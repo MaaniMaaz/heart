f7abf1b849d944c3264a7479d6eeba5f7d45f105
syedusmanhassan <usmanhasanwasti@gmail.com>
2025-04-04 16:53:28 +0500

    our mission section added in about page

--- src/About/Mission.jsx
+++ src/About/Mission.jsx
@@ -61,7 +61,7 @@ const FeatureSection = () => {
                 {feature.icon}
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white text-start font-['Raleway']">{feature.title}</h3>
-              <p className="text-gray-500 dark:text-gray-400 text-start font-['Raleway']">{feature.description}</p>
+              <p className="text-gray-500 dark:text-gray-400 text-start font-['Raleway'] ">{feature.description}</p>
             </div>
           ))}
         </div>
